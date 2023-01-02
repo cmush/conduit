c01cb90a43db88a1355b84d9d726c8205476dbfe
Collins Mucheru <cmush@users.noreply.github.com>
2023-01-02 23:04:08 +0300

    start conduit app in main application/sup

--- lib/conduit/application.ex
+++ lib/conduit/application.ex
@@ -17,9 +17,10 @@ defmodule Conduit.Application do
       # Start Finch
       {Finch, name: Conduit.Finch},
       # Start the Endpoint (http/https)
-      ConduitWeb.Endpoint
+      ConduitWeb.Endpoint,
       # Start a worker by calling: Conduit.Worker.start_link(arg)
-      # {Conduit.Worker, arg}
+      # {Conduit.Worker, arg},
+      Conduit.App
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
