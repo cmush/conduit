9b5f3ba869f5a7c9b443c4844aea298d91700353
Collins Mucheru <cmush@users.noreply.github.com>
2023-01-02 21:12:20 +0300

    create router & register with commanded app

---
+++ lib/conduit/router.ex
@@ -0,0 +1,8 @@
+defmodule Conduit.Router do
+  use Commanded.Commands.Router
+
+  alias Conduit.Accounts.{Aggregates.User, Commands.RegisterUser}
+
+  identify(User, by: :user_uuid)
+  dispatch([RegisterUser], to: User)
+end
